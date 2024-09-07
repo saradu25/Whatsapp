c53adfd86c982cc1807435e835ee9bc5a8a682ec
SaraDuran <saraduran174@gmail.com>
2024-09-07 17:32:29 -0300

    Cadastro - design

--- app/src/main/java/br/edu/ifsp/dmo/whatsapp/CadastroActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/whatsapp/CadastroActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.dmo.whatsapp.databinding.ActivityCadastroBinding
+import br.edu.ifsp.dmo.whatsapp.databinding.ActivityLoginBinding
 
 class CadastroActivity : AppCompatActivity() {
+
+    private val binding by lazy{
+        ActivityCadastroBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_cadastro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        inicializarToolbar()
+
+    }
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbar.tbPrincipal
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply{
+            title = "Faça o seu cadastro"
+            setDisplayHomeAsUpEnabled(true)
         }
     }
 }
--- app/src/main/java/br/edu/ifsp/dmo/whatsapp/LoginActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/whatsapp/LoginActivity.kt
@@ -1,20 +1,31 @@
 package br.edu.ifsp.dmo.whatsapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.dmo.whatsapp.databinding.ActivityLoginBinding
+import br.edu.ifsp.dmo.whatsapp.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy{
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        inicializarEventosClique()
+    }
+
+    private fun inicializarEventosClique(){
+        binding.textCadastro.setOnClickListener{
+            startActivity(
+                Intent(this, CadastroActivity::class.java)
+            )
         }
     }
+
 }
