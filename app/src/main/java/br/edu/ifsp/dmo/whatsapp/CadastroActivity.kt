7cdf1e37dfdf0c90bc5a1b8b34cdb91fc65ddb0d
SaraDuran <saraduran174@gmail.com>
2024-09-08 12:54:23 -0300

    Cadastro - validação de campos preenchidos

--- app/src/main/java/br/edu/ifsp/dmo/whatsapp/CadastroActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/whatsapp/CadastroActivity.kt
@@ -14,12 +14,52 @@ class CadastroActivity : AppCompatActivity() {
         ActivityCadastroBinding.inflate(layoutInflater)
     }
 
+    private lateinit var nome: String
+    private lateinit var email: String
+    private lateinit var senha: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         inicializarToolbar()
+        inicializarEventosClique()
+    }
 
+    private fun inicializarEventosClique() {
+        binding.btnCadastrar.setOnClickListener{
+            if(validarCampos()){
+                //criar usuario
+
+            }
+        }
+    }
+
+    private fun validarCampos(): Boolean {
+
+        nome = binding.editNome.text.toString()
+        email = binding.editEmail.text.toString()
+        senha =  binding.editSenha.text.toString()
+
+        if (nome.isNotEmpty()){
+            binding.textInputLayoutNome.error = null
+            if(email.isNotEmpty()){
+                binding.textInputLayoutEmail.error = null
+                if(senha.isNotEmpty()){
+                    binding.textInputLayoutSenha.error = null
+                    return true
+                }else {
+                    binding.textInputLayoutSenha.error = "Preencha a sua senha!"
+                    return false
+                }
+            }else{
+                binding.textInputLayoutEmail.error = "Preencha o seu email!"
+                return false
+            }
+        } else{
+            binding.textInputLayoutNome.error = "Preencha o seu nome!"
+            return false
+        }
     }
 
     private fun inicializarToolbar() {
