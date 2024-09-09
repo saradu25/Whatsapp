12e7f6a072e6df53c5623317cfc1253ba06da14c
SaraDuran <saraduran174@gmail.com>
2024-09-09 00:55:04 -0300

    Interface perfil criada, solicitação de permissões para usar camera e media.

--- app/src/main/java/br/edu/ifsp/dmo/whatsapp/PerfilActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/whatsapp/PerfilActivity.kt
@@ -1,20 +1,76 @@
 package br.edu.ifsp.dmo.whatsapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import br.edu.ifsp.dmo.whatsapp.databinding.ActivityPerfilBinding
+import br.edu.ifsp.dmo.whatsapp.databinding.ActivityCadastroBinding
 
 class PerfilActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityPerfilBinding.inflate( layoutInflater )
+    }
+    private var temPermissaoCamera = false
+    private var temPermissaoGaleria = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_perfil)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView( binding.root )
+        inicializarToolbar()
+        solicitarPermissoes()
+    }
+
+    private fun solicitarPermissoes() {
+
+        //Verifico se usuário já tem permissão
+        temPermissaoCamera = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+        temPermissaoGaleria = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_MEDIA_IMAGES
+        ) == PackageManager.PERMISSION_GRANTED
+
+        //PERMISSÕES NEGADAS
+        val listaPermissoesNegadas = mutableListOf<String>()
+        if( !temPermissaoCamera )
+            listaPermissoesNegadas.add( Manifest.permission.CAMERA )
+        if( !temPermissaoGaleria )
+            listaPermissoesNegadas.add( Manifest.permission.READ_MEDIA_IMAGES )
+
+        if( listaPermissoesNegadas.isNotEmpty() ){
+
+            //Solicitar multiplas permissões
+            val gerenciadorPermissoes = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ){ permissoes ->
+
+                temPermissaoCamera = permissoes[Manifest.permission.CAMERA]
+                    ?: temPermissaoCamera
+
+                temPermissaoGaleria = permissoes[Manifest.permission.READ_MEDIA_IMAGES]
+                    ?: temPermissaoGaleria
+
+            }
+            gerenciadorPermissoes.launch( listaPermissoesNegadas.toTypedArray() )
+
+        }
+
+    }
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbarPerfil.tbPrincipal
+        setSupportActionBar( toolbar )
+        supportActionBar?.apply {
+            title = "Editar perfil"
+            setDisplayHomeAsUpEnabled(true)
         }
     }
+
 }
